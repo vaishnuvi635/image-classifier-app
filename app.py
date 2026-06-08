401f79e18fdc685c52bef01dd690e891b85d5edf
vaishnuvi635 <shettarvaishnuvi635@gmail.com>
2026-06-09 01:41:21 +0530

    Refactor CNN image classification app
    Updated image preprocessing and classification display.

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 import streamlit as st
-import tensorflow as tf
 import numpy as np
+from tensorflow import keras
 from PIL import Image
 
-from tensorflow import keras
+# Load model
 model = keras.models.load_model("cnn_model.keras")
 
 classes = ["airplane","automobile","bird","cat","deer",
            "dog","frog","horse","ship","truck"]
 
-st.title("🧠 CNN Image Classification App")
+st.title("🧠 CNN Image Classifier")
 st.write("Upload an image and get prediction")
 
 file = st.file_uploader("Upload Image", type=["jpg","png","jpeg"])
@@ -18,10 +18,13 @@ if file:
     image = Image.open(file)
     st.image(image, caption="Uploaded Image", width=250)
 
-    img = image.resize((32,32))
-    img = np.array(img)/255.0
-    img = img.reshape(1,32,32,3)
+    # preprocess
+    img = image.convert("RGB")
+    img = img.resize((32, 32))
+    img = np.array(img) / 255.0
+    img = np.expand_dims(img, axis=0)
 
+    # prediction
     prediction = model.predict(img)
     result = classes[np.argmax(prediction)]
 
